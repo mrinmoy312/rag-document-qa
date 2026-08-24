d67dbfa804bea8fc331011a8c48ad23a7d9570ec
Mrinmoy Jana <mrinmoyjana5721@gmail.com>
2026-08-24 16:05:07 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -7,9 +7,10 @@ load_dotenv()
 LLM_PROVIDER = os.getenv("LLM_PROVIDER", "groq").lower()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
 GROQ_CHAT_MODEL = os.getenv(
     "GROQ_CHAT_MODEL",
-    "llama-3.3-70b-versatile"
+    "openai/gpt-oss-120b"
 )
 
 HF_EMBEDDING_MODEL = os.getenv(
@@ -18,9 +19,7 @@ HF_EMBEDDING_MODEL = os.getenv(
 )
 
 CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", "1000"))
-
 CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP", "200"))
-
 TOP_K = int(os.getenv("TOP_K", "4"))
 
 VECTOR_STORE_DIR = os.getenv(
